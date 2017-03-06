f09004c35d23acb38410367a3f9464bcb4f49606
Will James <wjames@thoughtworks.com>
2017-03-06 22:16:30 +1100

    Handle case when there are no ratings yet for a team or an opponent

--- src/adapter/predict.js
+++ src/adapter/predict.js
@@ -4,11 +4,15 @@ module.exports = function predict(teamId, opponentId, ratings) {
   let teamRating = ratings[teamId];
   let opponentRating = ratings[opponentId];
 
-  let llsGoalsFor = teamRating.offense - opponentRating.defense;
-  let llsGoalsAgainst = opponentRating.offense - teamRating.defense;
+  if (opponentRating && teamRating) {
+    let llsGoalsFor = teamRating.offense - opponentRating.defense;
+    let llsGoalsAgainst = opponentRating.offense - teamRating.defense;
 
-  return {
-    llsGoalsFor: llsGoalsFor,
-    llsGoalsAgainst: llsGoalsAgainst
-  };
+    return {
+      llsGoalsFor: llsGoalsFor,
+      llsGoalsAgainst: llsGoalsAgainst
+    };
+  } else {
+    return null;
+  }
 }
