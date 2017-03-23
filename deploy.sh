add52776c2cb1bc8074ff5f245331e45585911f5
Will James <wjames@thoughtworks.com>
2017-03-23 15:39:02 +1100

    Deploy to prod for master branch only.

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,10 @@ set -e
 source ./env/env.sh
 unset AWS_SESSION_TOKEN
 
-STAGE=dev
+if [ "$LAMBCI_BRANCH" = "master" ]; then
+  STAGE=prod
+else
+  STAGE=dev
+fi
 
 node_modules/.bin/serverless deploy --stage=$STAGE --verbose
