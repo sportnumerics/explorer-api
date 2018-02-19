f66bec79a4307bf3c89b46410e5978c3314a54c3
Will James <wjames@thoughtworks.com>
2018-02-19 10:53:25 -0600

    move to us-east-1

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,9 @@ unset AWS_SESSION_TOKEN
 
 if [ "$LAMBCI_BRANCH" = "master" ]; then
   pip install --user awscli
+  CDN_STACK_REGION="ap-southeast-2"
   CDN_STACK_NAME="sportnumerics-explorer-cdn-prod"
-  ACTIVE_DEPLOYMENT=$(aws cloudformation describe-stacks --stack-name $CDN_STACK_NAME --query 'Stacks[0].Outputs[?OutputKey==`ExplorerStageDeployment`].OutputValue' --output text)
+  ACTIVE_DEPLOYMENT=$(aws --region $CD_STACK_REGION cloudformation describe-stacks --stack-name $CDN_STACK_NAME --query 'Stacks[0].Outputs[?OutputKey==`ExplorerStageDeployment`].OutputValue' --output text)
   if [ "$ACTIVE_DEPLOYMENT" = "prodgreen" ]; then
     STAGE="prodblue"
   else
