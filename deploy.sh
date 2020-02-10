b293b73b1f7ff67adab76343e09b9e0523f48c36
Will James <jameswt@gmail.com>
2020-02-09 20:01:42 -0600

    Update deployment script

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,7 @@
 
 set -e
 
-./decrypt.sh
-source ./env/env.sh
-unset AWS_SESSION_TOKEN
-
 if [ "$LAMBCI_BRANCH" = "master" ]; then
-  pip install --user awscli
   ACTIVE_DEPLOYMENT=$(./node_modules/.bin/explorer-cdn describe-active-stage)
   if [ "$ACTIVE_DEPLOYMENT" = "prodgreen" ]; then
     STAGE="prodblue"
--- src/service/teams.js
+++ src/service/teams.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let utils = require('./utils');
-const config = require('config')
+const config = require('config');
 
 async function getTeamsByYearAndDiv(year, div) {
   const object = await utils.getObject({
@@ -10,14 +10,14 @@ async function getTeamsByYearAndDiv(year, div) {
   });
 
   return JSON.parse(object.Body);
-};
+}
 
 async function getTeamsByYearAndDivLocal(year, div) {
   const fs = require('fs').promises;
   const fileName = `${config.predictSrcDir}/divs/${div}.json`;
   console.log(`Getting ${fileName}`);
   const teams = await fs.readFile(fileName);
-return JSON.parse(teams);
+  return JSON.parse(teams);
 }
 
 module.exports = {
